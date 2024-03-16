9b12dbb1d74431ea549d8ab11af047a5d4818631
Ocean <hophuong.ocean@gmail.com>
2024-03-16 23:16:35 +0000

    added code for optimised bubble sort with unit tests

--- main.py
+++ main.py
@@ -20,3 +20,30 @@ def bubble_sort(array):
             i += 1
         n += 1
     return array
+
+def optimised_bubble_sort(array):
+    # define the first step
+    n = 0
+
+    # assign the length of the array to a var
+    length = len(array)
+
+    # initialise the swapped var
+    swapped = False
+
+    # initialise step of array
+    for n in range(length-1):
+        temp = array
+        for i in range(length-1-n):
+            first = array[i]
+            second = array[i+1]
+            if first > second:
+                array[i+1] = first
+                array[i] = second
+                # since there is swapping, we set swapped to True
+                swapped = True
+            i += 1
+        # after iterating through all elements, check if the array before is the same as the array after
+        if swapped is False:
+            return array
+    return array
--- test.py
+++ test.py
@@ -3,3 +3,7 @@ import main
 def test_bubble_sort():
     array = [-2, 45, 0, 11, -9]
     assert main.bubble_sort(array) == [-9, -2, 0, 11, 45]
+
+def test_optimised_bubble_sort():
+    array = [-2, 45, 0, 11, -9]
+    assert main.optimised_bubble_sort(array) == [-9, -2, 0, 11, 45]
