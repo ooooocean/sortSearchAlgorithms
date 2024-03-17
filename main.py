4597415369c43dedafca6de408f0c91a5bdc21a0
Ocean <hophuong.ocean@gmail.com>
2024-03-17 00:19:19 +0000

    linting

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ def bubble_sort(array):
     length = len(array)
 
     # we always iterate until
-    for n in range(length-1):
+    for n in range(length - 1):
 
-        # iterate over the array, whose length is determined by length-1 subtracted by the value of n,
-        # (number of largest elements)
-        for i in range(length-1-n):
+        # iterate over the array, whose length is determined by length-1 subtracted by
+        # the value of n, (number of largest elements)
+        for i in range(length - 1 - n):
             # assign the ith element to a var
             first = array[i]
-            second = array[i+1]
+            second = array[i + 1]
             if first > second:
-                array[i+1] = first
+                array[i + 1] = first
                 array[i] = second
             i += 1
         n += 1
     return array
+
 
 def optimised_bubble_sort(array):
     # define the first step
@@ -32,18 +33,32 @@ def optimised_bubble_sort(array):
     swapped = False
 
     # initialise step of array
-    for n in range(length-1):
-        temp = array
-        for i in range(length-1-n):
+    for n in range(length - 1):
+        for i in range(length - 1 - n):
             first = array[i]
-            second = array[i+1]
+            second = array[i + 1]
             if first > second:
-                array[i+1] = first
+                array[i + 1] = first
                 array[i] = second
                 # since there is swapping, we set swapped to True
                 swapped = True
             i += 1
-        # after iterating through all elements, check if the array before is the same as the array after
+        # after iterating through all elements, check if the array before is
+        # the same as the array after
         if swapped is False:
             return array
+    return array
+
+
+def selection_sort(array):
+    length = len(array)  # assign length of array to var
+
+    for n in range(length - 1):
+        minimum = n
+        for i in range(n, length - 1, 1):  # start from the new index
+            if array[minimum] > array[i+1]:
+                minimum = i+1
+        # once end of array reached, reassign start and end
+        array[n], array[minimum] = array[minimum], array[n]
+        n += 1
     return array
