b46eaa7dce6f9bc4be050e90444ca52f82f7a689
Ocean <hophuong.ocean@gmail.com>
2024-03-17 11:29:57 +0000

    linting

--- main.py
+++ main.py
@@ -56,12 +56,13 @@ def selection_sort(array):
     for n in range(length - 1):
         minimum = n
         for i in range(n, length - 1, 1):  # start from the new index
-            if array[minimum] > array[i+1]:
-                minimum = i+1
+            if array[minimum] > array[i + 1]:
+                minimum = i + 1
         # once end of array reached, reassign start and end
         array[n], array[minimum] = array[minimum], array[n]
         n += 1
     return array
+
 
 def insertion_sort(array):
     # start loop at step = 1
@@ -71,7 +72,59 @@ def insertion_sort(array):
         idx = step - 1
         # loop through
         while idx >= 0 and value < array[idx]:
-            array[idx+1] = array[idx]
+            array[idx + 1] = array[idx]
             idx -= 1
-        array[idx+1] = value
+        array[idx + 1] = value
     return array
+
+# define helper function that merges array after merge sort
+def merge(array, p, q, r):
+    # p and q determine subarray 1, where as q+1 and r determine subarray 2
+    # to merge these into a sorted array, first write the subarrays to a variable
+    subarray_1 = array[p:q+1:]
+    subarray_2 = array[q+1:r+1:]
+    # assign pointers for each of the arrays
+    idx_sub_1 = idx_sub_2 = idx_main = 0
+
+    # iterate until one subarray is complete
+    while idx_sub_1 != len(subarray_1) and idx_sub_2 != len(subarray_2):
+        # we add from subarray 1 if this element is larger, or the second subarray is iterated
+        if subarray_1[idx_sub_1] < subarray_2[idx_sub_2]:
+            array[idx_main] = subarray_1[idx_sub_1]
+            idx_sub_1 += 1
+            idx_main += 1
+        if subarray_2[idx_sub_2] < subarray_1[idx_sub_1]:
+            array[idx_main] = subarray_2[idx_sub_2]
+            idx_sub_2 += 1
+            idx_main += 1
+
+    # if subarray 1 is complete, iterate through second subarray
+    while idx_sub_1 != len(subarray_1):
+        array[idx_main] = subarray_1[idx_sub_1]
+        idx_sub_1 += 1
+        idx_main +=1
+
+    # repeat likewise for subarray 2
+    while idx_sub_2 != len(subarray_2):
+        array[idx_main] = subarray_2[idx_sub_2]
+        idx_sub_2 += 1
+        idx_main += 1
+
+    return array
+
+
+
+def merge_sort(array, p, r):
+    # define the terminal case
+    if p > r:
+        return
+
+    # find the midpoint of the input array. we always assign this to an int to drop the decimal
+    q = int((p + r) / 2)
+
+    # recurse function into first half
+    merge_sort(array[p:q+1:], p, q)
+    # recurse function into second half
+    merge_sort(array[q+1:r+1], q+1, r)
+    # we reach merge stage when a sub-array has a length 1
+    # merge(array, p, q, r)
--- test.py
+++ test.py
@@ -15,3 +15,13 @@ def test_selection_sort():
 def test_insertion_sort():
     array = [9, 5, 1, 4, 3]
     assert main.insertion_sort(array) == [1, 3, 4, 5, 9]
+
+def test_merge():
+    array = [7, 3]
+    assert main.merge(array,0, 0, 1) == [3,7]
+
+    array = [5,6,7,1]
+    assert main.merge(array,0,2,3) == [1,5,6,7]
+
+    array = [7,8,1,2,3]
+    assert main.merge(array,0,1,4) == [1,2,3,7,8]
